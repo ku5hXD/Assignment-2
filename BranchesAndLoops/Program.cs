4515b57b13cb25b814eab496740cf436d8953a93
ku5h <kushparmar4@gmail.com>
2022-01-24 16:31:49 +0530

    initial commit

---
+++ BranchesAndLoops/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Program
+{
+
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\n IF Demo : \n");
+            BranchesAndLoops.IFDemo();
+            Console.WriteLine("\n IF-ELSE Demo : \n");
+            BranchesAndLoops.IfElseDemo();
+            Console.WriteLine("\n WHILE and DO LOOP Demo : \n");
+            BranchesAndLoops.WhileAndDoLoopDemo();
+            Console.WriteLine("\n FOR LOOP Demo : \n");
+            BranchesAndLoops.ForLoopDemo();
+            Console.WriteLine("\n NESTED LOOP Demo : \n");
+            BranchesAndLoops.NestedLoopDemo();
+            Console.WriteLine("\n CONCLUSION : \n");
+            BranchesAndLoops.Conclusion();
+        }
+    }
+
+    static class BranchesAndLoops
+    {
+
+        public static void IFDemo()
+        {
+            int a = 5;
+            int b = 6;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10.");
+        }
+
+        public static void IfElseDemo()
+        {
+            int a = 5;
+            int b = 3;
+            int c = 4;
+            if ((a + b + c > 10) || (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not equal to the second");
+            }
+
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+        }
+
+        public static void WhileAndDoLoopDemo()
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Using While Loop The counter is {counter}");
+                counter++;
+            }
+
+            counter = 0;
+            do
+            {
+                Console.WriteLine($"Using DoWhile Loop The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+        }
+
+        public static void ForLoopDemo()
+        {
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Using For Loop , The counter is {counter}");
+            }
+        }
+
+        public static void NestedLoopDemo()
+        {
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($" cell -> ({row}, {column})");
+                }
+            }
+        }
+
+        public static void Conclusion()
+        {
+            int sum = 0;
+            for (int number = 1; number < 21; number++)
+            {
+                if (number % 3 == 0)
+                {
+                    sum = sum + number;
+                }
+            }
+            Console.WriteLine($"The sum is {sum}");
+        }
+
+    }
+}
---
+++ NumbersType/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https=//aka.ms/new-console-template for more information
+
+using System;
+
+namespace Program
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Numbers.IntDemo();
+            Numbers.DoubleDemo();
+            Numbers.DecimalDemo();
+            Numbers.findArea();
+        }
+    }
+
+    static class Numbers
+    {
+
+        public static void IntDemo()
+        {
+            int a = 18;
+            int b = 16;
+            int c = 4;
+            Console.WriteLine(" " + " " + "");
+            Console.WriteLine("\nOperations on Integers: ");
+            Console.WriteLine("\na+b = " + (a + b));
+            Console.WriteLine("a-b = " + (a - b));
+            Console.WriteLine("a*b = " + (a * b));
+            Console.WriteLine("a/b = " + (a / b));
+            Console.WriteLine("\nExplore Order of Operations: ");
+            Console.WriteLine("\na + b * c = " + (a + b * c));
+            Console.WriteLine("(a + b) * c = " + ((a + b) * c));
+            Console.WriteLine("(a + b) - 6 * c + (12 * 4) / 3 + 12 = " + ((a + b) - 6 * c + (12 * 4) / 3 + 12));
+            Console.WriteLine("(a + b) / c = " + ((a + b) / c) + "\n");
+            Console.WriteLine("Exploring integer precision and limits= \n");
+            Console.WriteLine($"quotient= {(a + b) / c}");
+            Console.WriteLine($"remainder= {(a + b) % c}\n");
+            Console.WriteLine($"The range of integers is {int.MaxValue} to {int.MinValue}\n");
+            Console.WriteLine("----------------------------------------------------------\n");
+        }
+
+        public static void DoubleDemo()
+        {
+            double a = 5;
+            double b = 65.67;
+            Console.WriteLine("Working with double : \n");
+            Console.WriteLine(("(a+b)/3.14 = " + (a + b) / 3.14) + "\n");
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}\n");
+            Console.WriteLine("1.0 / 3.0 = " + (1.0 / 3.0) + "\n");
+            Console.WriteLine("----------------------------------------------------------\n");
+        }
+
+        public static void DecimalDemo()
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine("Working with Decimal : \n");
+            Console.WriteLine($"\nThe range of the decimal type is {min} to {max}");
+            Console.WriteLine("\nDifference of precision between decimal and double: ");
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine("\nDouble = " + (a / b));
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine("\nDecimal = " + (c / d));
+            Console.WriteLine("----------------------------------------------------------\n");
+        }
+
+        public static void findArea()
+        {
+            double radius = 4.32;
+            double area = Math.PI * radius * radius;
+            Console.WriteLine($"area for radius {radius} = {area}\n");
+        }
+    }
+}
+
